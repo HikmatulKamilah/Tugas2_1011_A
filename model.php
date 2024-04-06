72e9f544933dc66d8a00ff16dd6416dc74cd1861
HikmatulKamilah <145831884+HikmatulKamilah@users.noreply.github.com>
2024-04-06 10:43:50 +0700

    Add files via upload

---
+++ model.php
@@ -0,0 +1,29 @@
+<?php
+class Contacts {
+    // Metode untuk melakukan SELECT
+    public static function select() {
+       
+    }
+
+    // Metode untuk melakukan INSERT
+    public static function insert() {
+
+    }
+
+    // Metode untuk melakukan UPDATE
+    public static function update() {
+       
+    }
+
+    // Metode untuk melakukan DELETE
+    public static function delete() {
+        
+    }
+    
+    // Properti atau atribut dari kelas Contacts
+    public $ID;
+    public $nomor_telepon;
+    public $nama;
+    public $alamat;
+}
+?>
